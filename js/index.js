75356b1c2dd8c12359fe05aec203cb36f585fed6
vivek <vivekb@hashcube.com>
2017-11-28 14:32:29 +0530

    updated adapaters admob, facebook, chartboost and ironsource

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ var is_rv_available = false;
  */
 var is_ow_available = false;
 
-var Ironsource = Class(Emitter, function (supr) {
+var IronSource = Class(Emitter, function (supr) {
   this.init = function() {
     supr(this, 'init', arguments);
 
@@ -88,21 +88,21 @@ var Ironsource = Class(Emitter, function (supr) {
       }
     });
 
-    NATIVE.events.registerHandler("ironsourceAdDismissed", function() {
+    NATIVE.events.registerHandler("ironsSAdDismissed", function() {
       logger.log("{ironSource} ad dismissed ");
       if (typeof onAdDismissed === "function") {
         onAdDismissed();
       }
     });
 
-    NATIVE.events.registerHandler("ironsourceAdAvailable", function() {
+    NATIVE.events.registerHandler("ironSourceAdAvailable", function() {
       logger.log("{ironSource} ad available");
       if (typeof onAdAvailable === "function") {
         onAdAvailable("ironSource");
       }
     });
 
-    NATIVE.events.registerHandler("ironsourceAdNotAvailable", function() {
+    NATIVE.events.registerHandler("ironSourceAdNotAvailable", function() {
       logger.log("{ironSource} ad not available");
       if (typeof onAdNotAvailable === "function") {
         onAdNotAvailable();
@@ -123,12 +123,12 @@ var Ironsource = Class(Emitter, function (supr) {
       }
     };
 
-    NATIVE.events.registerHandler('ironsourceRVAdClosed', function(evt) {
+    NATIVE.events.registerHandler('IronSourceRVAdClosed', function(evt) {
       this.onVideoClosed(rv_source, evt.placement, evt.rewardedCount);
       rv_source = null;
     });
 
-    NATIVE.events.registerHandler('ironsourceOnRVAvailabilityChange', function(evt) {
+    NATIVE.events.registerHandler('ironSourceOnRVAvailabilityChange', function(evt) {
       onRWAvailabilityChange(evt.available);
     });
 
@@ -148,11 +148,11 @@ var Ironsource = Class(Emitter, function (supr) {
   };
 
   this.showInterstitial = function() {
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "showInterstitial", JSON.stringify({}));
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "showInterstitial", JSON.stringify({}));
   };
 
   this.cacheInterstitial = function() {
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "cacheInterstitial", JSON.stringify({}));
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "cacheInterstitial", JSON.stringify({}));
   }
 
   this.isOWAdAvailable = function() {
@@ -167,7 +167,7 @@ var Ironsource = Class(Emitter, function (supr) {
   this.initInterstitial = function(user_id) {
     this.user_id = user_id;
 
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "initInterstitial", JSON.stringify({
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "initInterstitial", JSON.stringify({
       user_id: user_id
     }));
   };
@@ -175,13 +175,13 @@ var Ironsource = Class(Emitter, function (supr) {
   this.initVideoAd = function(user_id) {
     this.user_id = user_id;
 
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "initVideoAd", JSON.stringify({
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "initVideoAd", JSON.stringify({
       user_id: user_id
     }));
   };
 
   this.initOfferWallAd = function(user_id) {
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "initOfferWallAd", JSON.stringify({
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "initOfferWallAd", JSON.stringify({
       user_id: user_id
     }));
   };
@@ -189,16 +189,16 @@ var Ironsource = Class(Emitter, function (supr) {
   this.showVideoAd = function(placement_name) {
     rv_source = placement_name;
 
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "showRVAd", JSON.stringify({
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "showRVAd", JSON.stringify({
       placementName: placement_name
     }));
   };
 
   this.showOWAd = function(userid) {
-    NATIVE.plugins.sendEvent("IronsourcePlugin", "showOffersForUserID", JSON.stringify({
+    NATIVE.plugins.sendEvent("IronSourcePlugin", "showOffersForUserID", JSON.stringify({
       userID: userid
     }));
   };
 });
 
-exports = new Ironsource();
+exports = new IronSource();
